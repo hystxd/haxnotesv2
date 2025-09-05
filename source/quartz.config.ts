ca709568294c8be569e4744ba2f567d72a19e468
hystxd <allenseven777@gmail.com>
2025-09-06 02:50:44 +1200

    vault backup: 2025-09-06 02:50:44

--- source/quartz.config.ts
+++ source/quartz.config.ts
@@ -8,7 +8,7 @@ import * as Plugin from "./quartz/plugins"
  */
 const config: QuartzConfig = {
   configuration: {
-    pageTitle: "allenxd",
+    pageTitle: "👾 allenxd",
     pageTitleSuffix: "",
     enableSPA: true,
     enablePopovers: true,
